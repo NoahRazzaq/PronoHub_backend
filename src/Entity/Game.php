7bb96ce107c698c263765d058f23fb0e62e16d03
NoahRazzaq <noahrazzaq@live.fr>
2023-11-21 10:23:31 +0100

    first commit

---
+++ src/Entity/Game.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $score1 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $score2 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $banner = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScore1(): ?int
+    {
+        return $this->score1;
+    }
+
+    public function setScore1(?int $score1): static
+    {
+        $this->score1 = $score1;
+
+        return $this;
+    }
+
+    public function getScore2(): ?int
+    {
+        return $this->score2;
+    }
+
+    public function setScore2(?int $score2): static
+    {
+        $this->score2 = $score2;
+
+        return $this;
+    }
+
+    public function getBanner(): ?string
+    {
+        return $this->banner;
+    }
+
+    public function setBanner(string $banner): static
+    {
+        $this->banner = $banner;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
